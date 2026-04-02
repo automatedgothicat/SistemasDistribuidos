97345cf223be5c6c1cb923b0294d492d7154cb98
Automated gothic cat <163703962+automatedgothicat@users.noreply.github.com>
2026-04-01 20:09:53 -0400

    Update app_cliente.py

--- app_cliente.py
+++ app_cliente.py
@@ -27,12 +27,34 @@ def matricula(disciplina, nome_aluno):
     print(saida.status_code, saida.json())
 
 if __name__ == "__main__":
-    #status_server()
-    #listar_disciplinas()
-    #matricula("Sistemas Distribuídos","Ciclano Souza")
-    #matricula("Sistemas Distribuídos","Fulano da Silva")
-    #matricula("Sistemas Distribuídos","Beltrano Alves")
-    #listar_disciplinas()
-    #matriculados_disciplina("SD")
-    nome_disciplina = input("Insira o nome da disciplina:")
-    nome_aluno = input("Insira o nome do aluno:")
+    status_server()
+    # listar_disciplinas()
+    # matricula("Sistemas Distribuídos","Ciclano Souza")
+    # matricula("Sistemas Distribuídos","Fulano da Silva")
+    # matricula("Sistemas Distribuídos","Beltrano Alves")
+    # listar_disciplinas()
+    # matriculados_disciplina("SD")
+
+    ans = 0
+    while ans != 0:
+        print('''
+              0 - Fechar/sair
+              1 - Listar disciplinas
+              2 - Listar matriculados em uma disciplina
+              3 - Matricular em uma disciplina
+        ''')
+        ans = input("Insira o número da ação desejada:")
+
+        if ans == "1":
+            listar_disciplinas()
+        elif ans == "2":
+            codigo = input("Insira o código da disciplina:")
+            matriculados_disciplina(codigo)
+        elif ans == "3":
+            nome_disciplina = input("Insira o nome da disciplina:")
+            nome_aluno = input("Insira o nome do aluno:")
+            matricula(nome_disciplina,nome_aluno)
+            print("Cadastrando ", nome_aluno, " em ", nome_disciplina)
+        else:
+            print("Tente novamente")
+    print("Programa encerrado")
